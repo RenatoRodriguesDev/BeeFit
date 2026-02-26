5f21a5d12f2e7cb602604054d689b28d35a7f22f
Renato Rodrigues <renato-silva_16@hotmail.com>
2026-02-25 22:28:49 -0500

    added toastyfy.js

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,5 +1,9 @@
 import './bootstrap';
+
 import Sortable from 'sortablejs';
+
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
 
 import Alpine from 'alpinejs';
 
@@ -36,4 +40,24 @@ function initSortable() {
             Livewire.dispatch('reorderExercises', { order });
         }
     });
+}
+
+window.toast = function(message, type = 'success') {
+
+    let bg = type === 'success'
+        ? "linear-gradient(to right, #10b981, #059669)"
+        : "linear-gradient(to right, #ef4444, #dc2626)";
+
+    Toastify({
+        text: message,
+        duration: 3000,
+        gravity: "top",
+        position: "right",
+        style: {
+            background: bg,
+            color: "white",
+            borderRadius: "14px",
+            padding: "12px 18px"
+        }
+    }).showToast();
 }
