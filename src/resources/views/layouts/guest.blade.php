079f2b5c5def0968f2048ba0559d5b2cbca00d1c
Renato Rodrigues <renato-silva_16@hotmail.com>
2026-02-26 17:42:42 -0500

    no logout favicon changed

--- src/resources/views/layouts/guest.blade.php
+++ src/resources/views/layouts/guest.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
