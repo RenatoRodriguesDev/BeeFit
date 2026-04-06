0b608eb1f40ca55e5860748a8cc52ffd6e132486
Renato Rodrigues <renato-silva_16@hotmail.com>
2026-04-06 00:24:38 -0500

    added exercises seeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -19,9 +19,7 @@ class DatabaseSeeder extends Seeder
         }
         $this->call([
             UsersTableSeeder::class,
-            MuscleSeeder::class,
-            EquipmentSeeder::class,
-            ExerciseSeeder::class
+            ExercisesSeeder::class
         ]);
     }
 }
